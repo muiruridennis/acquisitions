7185d8f4a481262f32bdcc699ca5cf41811e81ee
Dennis Muiruri <m.dennismuiruri@gmail.com>
2025-11-05 10:18:17 +0300

    fixed linting

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,12 +1,11 @@
-import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, timestamp } from 'drizzle-orm/pg-core';
 
-export const users = pgTable("users", {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 255 }).notNull(),
-    email: varchar("email", { length: 255 }).notNull().unique(),
-    password: varchar("password", { length: 255 }).notNull(),
-    role: varchar("role", { length: 50 }).notNull().default("user"),
-    created_at: timestamp().notNull().defaultNow(),
-    updated_at: timestamp().notNull().defaultNow(),
-    
-})
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  password: varchar('password', { length: 255 }).notNull(),
+  role: varchar('role', { length: 50 }).notNull().default('user'),
+  created_at: timestamp().notNull().defaultNow(),
+  updated_at: timestamp().notNull().defaultNow(),
+});
